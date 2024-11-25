45a2ed97f38b1c8a997ac3f7806120d3cc464480
Thomas Rodriguez <trodriguez91@icloud.com>
2024-11-24 22:47:50 -0700

    add correct compilation setup for linux

--- lib/native.ex
+++ lib/native.ex
@@ -9,24 +9,30 @@ defmodule JupSwap.Native do
     legacy_cpu: fn ->
       # These are the same from the release workflow.
       # See the meaning in: https://unix.stackexchange.com/a/43540
-      _needed_caps = ~w[fxsr sse sse2 ssse3 sse4_1 sse4_2 popcnt avx fma]
-      # TODO: Implement logic using _needed_caps
-      :ok
+      needed_caps = ~w[fxsr sse sse2 ssse3 sse4_1 sse4_2 popcnt avx fma]
+
+      
+      not Unit.ComptimeUtils.cpu_with_all_caps?(needed_caps)
     end
   ]
-
-  # other_variants = [legacy_cpu: fn -> use_legacy end]
 
   use RustlerPrecompiled,
     otp_app: :jup_swap,
     version: version,
-    base_url: "#{github_url}/releases/download/v#{version}"|>IO.inspect(),
+    base_url: "#{github_url}/releases/download/v#{version}",
     targets: ~w(
+      aarch64-apple-darwin
+      aarch64-unknown-linux-gnu
+      aarch64-unknown-linux-musl
       x86_64-apple-darwin
+      x86_64-pc-windows-msvc
+      x86_64-pc-windows-gnu
       x86_64-unknown-linux-gnu
+      x86_64-unknown-linux-musl
+      x86_64-unknown-freebsd
     ),
     variants: %{
-      "x86_64-unknown-linux-gnu" => variants_for_linux
+      "x86_64-unknown-linux-gnu" => variants_for_linux,
     },
     # We don't use any features of newer NIF versions, so 2.15 is enough.
     nif_versions: ["2.15"],
