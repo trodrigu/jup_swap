3be356d5323511190651f53b17242a72b72b6cfc
Thomas Rodriguez <trodriguez91@icloud.com>
2025-01-18 20:14:06 -0700

    rev

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule JupSwap.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/trodrigu/jup_swap"
-  @version "0.1.9"
+  @version "0.2.0"
   @dev? String.ends_with?(@version, "-dev")
   @force_build? System.get_env("JUP_SWAP_BUILD") in ["1", "true"]
 
